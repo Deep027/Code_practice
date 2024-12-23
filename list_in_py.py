bdd10b6339d24708bcd2c2e4d349449cf52f6393
Deep027 <deepjariwala027@gmail.com>
2024-12-23 13:31:33 -0500

    test push

---
+++ list_in_py.py
@@ -0,0 +1,47 @@
+a = [10, 20, 15]
+print(a)
+
+# Creating a List 
+# Here are some common methods to create a list: 
+# Using Square Brackets
+
+# List of integers
+a = [1, 2, 3, 4, 5]
+
+# List of strings
+b = ['apple', 'banana', 'cherry']
+
+# Mixed data types
+c = [1, 'hello', 3.14, True]
+
+print(a)
+print(b)
+print(c)
+
+# Using list() Constructor
+# From a tuple
+a = list((1, 2, 3, 'apple', 4.5))  
+
+print(a)
+
+# Creating List with Repeated Elements
+# Create a list [2, 2, 2, 2, 2]
+a = [2] * 5
+
+# Create a list [0, 0, 0, 0, 0, 0, 0]
+b = [0] * 7
+
+print(a)
+print(b)
+
+# Accessing List Elements
+# Elements in a list can be accessed using indexing. 
+# Python indexes start at 0, so a[0] will access the first element, while negative indexing allows us to access elements from the end of the list. 
+# Like index -1 represents the last elements of list.
+a = [10, 20, 30, 40, 50]
+
+# Access first element
+print(a[0])    
+
+# Access last element
+print(a[-1])
